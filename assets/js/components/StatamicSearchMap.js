2a89c5f40835bb77d763802db116551703420e6e
Adam Tsai <adam@visualchefs.com>
2019-08-28 10:04:38 -0400

    Add automatic map bounds based on Statamic search results

--- assets/js/components/StatamicSearchMap.js
+++ assets/js/components/StatamicSearchMap.js
@@ -1,13 +1,19 @@
 import React, { useReducer } from 'react';
 import ReactMapGL, { Marker, NavigationControl } from 'react-map-gl';
+import PropTypes from 'prop-types';
+import useDeepCompareEffect from 'use-deep-compare-effect';
+import WebMercatorViewport from 'viewport-mercator-project';
 import mapStyle from './style.json';
 import HitIcon from './HitIcon';
 import viewportReducer from './reducers/viewportReducer';
 
 const token = process.env.REACT_APP_MAPBOX_TOKEN;
-const classNames = require('classnames');
 
 const StatamicSearchMap = ({ searchResults, height, width }) => {
+    //----------------------------
+    // State
+    //----------------------------
+
     const [viewport, dispatch] = useReducer(viewportReducer, {
         width,
         height,
@@ -16,7 +22,63 @@ const StatamicSearchMap = ({ searchResults, height, width }) => {
         zoom: 1,
     });
 
+    //----------------------------
+    // Helpers
+    //----------------------------
+
     const onUpdateViewport = updatedViewport => dispatch({ type: 'UPDATE', params: updatedViewport });
+    const getBoundingBoxFromSearchResult = result => ({
+        longitude: Number(result.longitude),
+        latitude: Number(result.latitude),
+        zoom: 9,
+    });
+
+    const getBoundingBoxFromSearchResults = results => {
+        if (results.length === 1) {
+            return getBoundingBoxFromSearchResult(results[0]);
+        }
+
+        const lats = results.map(result => result.latitude);
+        const lngs = results.map(result => result.longitude);
+        const maxLng = lngs.reduce((a, b) => Math.max(a, b));
+        const minLng = lngs.reduce((a, b) => Math.min(a, b));
+        const maxLat = lats.reduce((a, b) => Math.max(a, b));
+        const minLat = lats.reduce((a, b) => Math.min(a, b));
+        const bounds = [[minLng, minLat], [maxLng, maxLat]];
+
+        try {
+            return new WebMercatorViewport(viewport).fitBounds(bounds, {
+                padding: 20,
+                offset: [-100, -100],
+            });
+        } catch (error) {
+            console.log(error);
+        }
+    };
+
+    //----------------------------
+    // Effects
+    //----------------------------
+
+    useDeepCompareEffect(() => {
+        if (searchResults.length === 0) {
+            return;
+        }
+
+        const { longitude, latitude, zoom } = getBoundingBoxFromSearchResults(searchResults);
+        dispatch({
+            type: 'UPDATE',
+            params: {
+                longitude,
+                latitude,
+                zoom,
+            },
+        });
+    }, [searchResults]);
+
+    //----------------------------
+    // Render
+    //----------------------------
 
     return (
         <ReactMapGL {...viewport} mapStyle={mapStyle} mapboxApiAccessToken={token} onViewportChange={onUpdateViewport}>
@@ -38,7 +100,13 @@ const StatamicSearchMap = ({ searchResults, height, width }) => {
                 ))}
             </div>
         </ReactMapGL>
-    )
+    );
+};
+
+StatamicSearchMap.propTypes = {
+    searchResults: PropTypes.array.isRequired,
+    height: PropTypes.number.isRequired,
+    width: PropTypes.number.isRequired,
 };
 
 export default StatamicSearchMap;
