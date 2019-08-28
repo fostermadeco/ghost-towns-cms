06441fc4d0e8f9f3912e02e4c923348f0669864d
Adam Tsai <adam@visualchefs.com>
2019-08-28 10:48:57 -0400

    Update formatting with Prettier and add prop validation

--- assets/js/components/StatamicSearchExplorePage.js
+++ assets/js/components/StatamicSearchExplorePage.js
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
 
 // App
 import StatamicSearchMap from './StatamicSearchMap';
@@ -14,12 +15,7 @@ import {
     reducers,
 } from './reducers/StatamicSearchExplorePageReducer';
 
-const StatamicSearchExplorePageComponent = ({
-    searchResults,
-    statesList,
-    fetchSearchResults,
-    fetchStates,
-}) => {
+const StatamicSearchExplorePageComponent = ({ searchResults, statesList, fetchSearchResults, fetchStates }) => {
     //----------------------------
     // State
     //----------------------------
@@ -31,7 +27,7 @@ const StatamicSearchExplorePageComponent = ({
     // Helpers
     //----------------------------
 
-    const search = async (e) => {
+    const search = async e => {
         e.preventDefault();
         fetchSearchResults(searchTerm);
     };
@@ -47,7 +43,7 @@ const StatamicSearchExplorePageComponent = ({
     useEffect(() => {
         fetchSearchResults('');
         fetchStates();
-    }, []);
+    }, [fetchSearchResults, fetchStates]);
 
     //----------------------------
     // Render
@@ -69,16 +65,28 @@ const StatamicSearchExplorePageComponent = ({
                             required
                             maxLength="512"
                             value={searchTerm}
-                            onChange={(event) => setSearchTerm(event.target.value)}
+                            onChange={event => setSearchTerm(event.target.value)}
                         />
                         <button type="submit" title="Submit your search query." className="ais-SearchBox-submit">
-                            <svg className="ais-SearchBox-submitIcon" xmlns="http://www.w3.org/2000/svg" width="10" height="10" viewBox="0 0 40 40">
-                                <path d="M26.804 29.01c-2.832 2.34-6.465 3.746-10.426 3.746C7.333 32.756 0 25.424 0 16.378 0 7.333 7.333 0 16.378 0c9.046 0 16.378 7.333 16.378 16.378 0 3.96-1.406 7.594-3.746 10.426l10.534 10.534c.607.607.61 1.59-.004 2.202-.61.61-1.597.61-2.202.004L26.804 29.01zm-10.426.627c7.323 0 13.26-5.936 13.26-13.26 0-7.32-5.937-13.257-13.26-13.257C9.056 3.12 3.12 9.056 3.12 16.378c0 7.323 5.936 13.26 13.258 13.26z"></path>
+                            <svg
+                                className="ais-SearchBox-submitIcon"
+                                xmlns="http://www.w3.org/2000/svg"
+                                width="10"
+                                height="10"
+                                viewBox="0 0 40 40"
+                            >
+                                <path d="M26.804 29.01c-2.832 2.34-6.465 3.746-10.426 3.746C7.333 32.756 0 25.424 0 16.378 0 7.333 7.333 0 16.378 0c9.046 0 16.378 7.333 16.378 16.378 0 3.96-1.406 7.594-3.746 10.426l10.534 10.534c.607.607.61 1.59-.004 2.202-.61.61-1.597.61-2.202.004L26.804 29.01zm-10.426.627c7.323 0 13.26-5.936 13.26-13.26 0-7.32-5.937-13.257-13.26-13.257C9.056 3.12 3.12 9.056 3.12 16.378c0 7.323 5.936 13.26 13.258 13.26z" />
                             </svg>
                         </button>
                         <button type="reset" title="Clear the search query." className="ais-SearchBox-reset" hidden="">
-                            <svg className="ais-SearchBox-resetIcon" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" width="10" height="10">
-                                <path d="M8.114 10L.944 2.83 0 1.885 1.886 0l.943.943L10 8.113l7.17-7.17.944-.943L20 1.886l-.943.943-7.17 7.17 7.17 7.17.943.944L18.114 20l-.943-.943-7.17-7.17-7.17 7.17-.944.943L0 18.114l.943-.943L8.113 10z"></path>
+                            <svg
+                                className="ais-SearchBox-resetIcon"
+                                xmlns="http://www.w3.org/2000/svg"
+                                viewBox="0 0 20 20"
+                                width="10"
+                                height="10"
+                            >
+                                <path d="M8.114 10L.944 2.83 0 1.885 1.886 0l.943.943L10 8.113l7.17-7.17.944-.943L20 1.886l-.943.943-7.17 7.17 7.17 7.17.943.944L18.114 20l-.943-.943-7.17-7.17-7.17 7.17-.944.943L0 18.114l.943-.943L8.113 10z" />
                             </svg>
                         </button>
                     </form>
@@ -118,6 +126,14 @@ const StatamicSearchExplorePageComponent = ({
     );
 };
 
+StatamicSearchExplorePageComponent.propTypes = {
+    // Redux
+    searchResults: PropTypes.array.isRequired,
+    statesList: PropTypes.array.isRequired,
+    fetchSearchResults: PropTypes.func.isRequired,
+    fetchStates: PropTypes.func.isRequired,
+};
+
 const mapStateToProps = state => ({
     searchResults: getSearchResultsState(state),
     statesList: getStatesListState(state),
@@ -128,7 +144,11 @@ const mapDispatchToProps = dispatch => ({
     fetchStates: (...args) => dispatchFetchStates(...args)(dispatch),
 });
 
-const StatamicSearchExplorePage = connect(mapStateToProps, mapDispatchToProps)(StatamicSearchExplorePageComponent);
+const StatamicSearchExplorePage = connect(
+    mapStateToProps,
+    mapDispatchToProps
+)(StatamicSearchExplorePageComponent);
+
 StatamicSearchExplorePage.reducers = {
     StatamicSearchExplorePage: reducers,
 };
