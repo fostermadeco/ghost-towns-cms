9b5f54ffe490552b861f6bff332f6f2c3d5753a9
Adam Tsai <adam@visualchefs.com>
2019-09-04 08:51:37 -0400

    Add temporary functionality for autosuggest box

--- assets/js/components/StatamicSearchAutoSuggestPage.js
+++ assets/js/components/StatamicSearchAutoSuggestPage.js
@@ -3,35 +3,67 @@ import Autosuggest from 'react-autosuggest';
 import { connect } from 'react-redux';
 
 // App
-import {
-    reducers,
-} from './reducers/StatamicSearchAutoSuggestPageReducer';
+import { reducers } from './reducers/StatamicSearchAutoSuggestPageReducer';
+
+const languages = [
+    {
+        name: 'C',
+        year: 1972,
+    },
+    {
+        name: 'Elm',
+        year: 2012,
+    },
+];
 
 const StatamicSearchAutoSuggestPageComponent = () => {
-    useEffect(() => {
-        // 
-    }, []);
+    //----------------------------
+    // State
+    //----------------------------
+
+    const [searchText, setSearchText] = useState('');
+
+    //----------------------------
+    // Functions
+    //----------------------------
+
+    const onSuggestionsFetchRequested = () => {
+        //
+    };
+
+    const onSuggestionsClearRequested = () => {
+        //
+    };
+
+    const getSuggestionValue = () => {
+        //
+    };
+
+    //----------------------------
+    // Render
+    //----------------------------
 
     const inputProps = {
-        value: '',
-        onChange: () => { }
-    }
+        value: searchText,
+        onChange: (event) => setSearchText(event.target.value),
+    };
 
     return (
         <div className="flex flex-wrap">
             <div className="w-full md:w-1/2">
                 <div className="mx-2 mt-3">
                     <Autosuggest
-                        suggestions={[]}
-                        onSuggestionsFetchRequested={() => { }}
-                        onSuggestionsClearRequested={() => { }}
-                        getSuggestionValue={() => { }}
-                        renderSuggestion={suggestion => (<span>{suggestion.name}</span>)}
-                        inputProps={inputProps} />
+                        suggestions={languages}
+                        onSuggestionsFetchRequested={onSuggestionsFetchRequested}
+                        onSuggestionsClearRequested={onSuggestionsClearRequested}
+                        getSuggestionValue={getSuggestionValue}
+                        renderSuggestion={suggestion => <span>{suggestion.name}</span>}
+                        inputProps={inputProps}
+                    />
                 </div>
             </div>
 
-            <div className="w-full md:w-1/2"></div>
+            <div className="w-full md:w-1/2" />
         </div>
     );
 };
